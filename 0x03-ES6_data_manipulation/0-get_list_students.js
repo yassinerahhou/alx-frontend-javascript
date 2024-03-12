a809804f5a02990daf12ce711100cb82a52e6046
yassine rahhou <125989457+yassinerahhou@users.noreply.github.com>
2024-03-12 22:41:22 +0000

    fix errores

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,10 @@
 export default getListStudents;
 
-function getListStudents {
-    const myArray = [ 
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-
-];
-return myArray
+function getListStudents() {
+  const myArray = [
+    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    { id: 2, firstName: "James", location: "Columbia" },
+    { id: 5, firstName: "Serena", location: "San Francisco" },
+  ];
+  return myArray;
 }
