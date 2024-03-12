da544851bf58f61f22007788cbfeb1995a9ea0e3
yassinerahhou <yassine.rahhou.1@gmail.com>
2024-03-12 23:06:31 +0000

    fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function getListStudents() {
   const myArray = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
@@ -8,14 +7,3 @@ function getListStudents() {
   return myArray;
 }
 export default getListStudents;
-=======
-function getListStudents() {
-  const myArray = [
-    { id: 1, firstName: "Guillaume", location: "San Francisco" },
-    { id: 2, firstName: "James", location: "Columbia" },
-    { id: 5, firstName: "Serena", location: "San Francisco" },
-  ];
-  return myArray;
-}
-export default getListStudents;
->>>>>>> e479995b45278457933fe58f7eef2413958bf361
